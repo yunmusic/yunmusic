3e27417bc1e7d4b3d14c35be2c1b2b1e8642c774
andthen <727031052@qq.com>
2019-03-09 20:03:46 +0800

    根据id查询歌曲所有相关信息

--- src/main/java/com/yun/music/dao/MusicMapper.java
+++ src/main/java/com/yun/music/dao/MusicMapper.java
@@ -1,6 +1,7 @@
 package com.yun.music.dao;
 
 import com.yun.music.entity.Music;
+import com.yun.music.vo.MusicVo;
 
 public interface MusicMapper {
     int deleteByPrimaryKey(Integer id);
@@ -10,8 +11,11 @@ public interface MusicMapper {
     int insertSelective(Music record);
 
     Music selectByPrimaryKey(Integer id);
+    //通过id查找歌曲所有的相关信息
+    MusicVo selectById(int id);
 
     int updateByPrimaryKeySelective(Music record);
 
     int updateByPrimaryKey(Music record);
+
 }
